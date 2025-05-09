dc06017dde6fb4d08893d9b5e630d5426e57ed7f
Suyesh Rimal <102273933+suyeshrimal@users.noreply.github.com>
2025-05-10 01:20:37 +0545

    Time: 0 ms (100%), Space: 17.8 MB (50.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,8 +8,8 @@ class Solution:
             if char in opening_brackets:
                 stack.append(char)
             elif char in closing_brackets:
-                if not stack or stack[-1]!=brackets_dic[char]:
+                if len(stack)==0 or stack[-1]!=brackets_dic[char]:
                     return False
                 stack.pop()
 
-        return not stack
+        return len(stack)==0
