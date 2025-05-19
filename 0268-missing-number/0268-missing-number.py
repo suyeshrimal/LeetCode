77055bb8d26216dc4e5bf87de2e251757f8bfb9a
Suyesh Rimal <102273933+suyeshrimal@users.noreply.github.com>
2025-05-19 22:50:55 +0545

    Time: 1296 ms (10.16%), Space: 18.6 MB (72.71%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        max_num=max(nums)
-        for i in range(0,max_num+2):
+        
+        for i in range(0,len(nums)+1):
             if i not in nums:
                 return i
